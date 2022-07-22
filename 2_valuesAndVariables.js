1241cef278f1a8bcc8f81b262162a0ddebc852ca
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-22 15:13:03 -0400

    Add section 2

---
+++ 2_valuesAndVariables.js
@@ -0,0 +1,51 @@
+// numbers for all types of numbers 
+1.0000
+1
+15
+// there is a maxium size for numbers
+
+// Nan stands for not a number
+NaN 
+
+// variables 
+let someName = value; 
+let age = 72; 
+let hens = 4; 
+let roosters = 2; 
+hens + roosters // 6 
+hens - 1; // 3
+//hens still equals 4
+hens = hens - 1; 
+hens // 3
+// now hens = 3
+// name variables using CamelCase
+let numOfHens = 10;
+let avgRating = 9.7;
+
+// cannot redo let within the same scope, must reassign 
+
+let score = 0; 
+score += 1;   
+score += 1;   
+score += 1;  
+//score is 3  
+score -= 1; 
+//score is 2 
+let bonusPoints = 3; 
+score *= bonusPoints; 
+//score is 6
+let counter = 0; 
+counter++; 
+//counter = 1
+counter--;
+//counter = 0
+//Unary operator
+
+
+//const works just like let but you cannot change it
+const year = 1999; 
+// can use it for pi  
+// days in the week
+// minimim height for a ride 
+// USE IT FOR THINGS THAT DO NOT CHANGE
+
