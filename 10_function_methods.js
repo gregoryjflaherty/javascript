85c91a6e8a75f7d018ba737c88af957a31fa757c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-01 17:13:29 -0400

    Functions - forEach

--- 10_function_methods.js
+++ 10_function_methods.js
@@ -6,3 +6,46 @@
 // reduce 
 // some 
 // every 
+
+// forEach 
+const nums = [1, 4, 5, 67 , 8]
+
+
+nums.forEach(function(n) {
+    console.log(n * 2);
+}) // anonymous function
+
+function printTriples(n) {
+    console.log(n * 3)
+}
+nums.forEach(printTriples); 
+
+const books = [{
+        title: 'Good Omens', 
+        authors: ['Terry Pratchett', 'Neil Gaiman'],
+        rating: 4.25
+    }, 
+    {
+        title: 'Bone: The Complete Collection',
+        authors: ['Jeff Smith'],
+        rating: 4.42
+    }, 
+    {
+        title: 'American Gods', 
+        authors: ['Neil Gaiman'],
+        rating: 4.11
+    },
+    {
+        title: 'A Gentleman in Moscow',
+        authors: ['Amor Towles'],
+        rating: 4.36
+    }]
+
+books.forEach(function(book) {
+    console.log(book.title.toUpperCase());
+})
+
+// w/ index 
+books.forEach(function(book, idx) {
+    console.log(book.title, idx);
+})
